5b50ec5578e823b6d343ea36179c2700c1391122
Stone <zyshi21@cse.cuhk.edu.hk>
2024-03-05 19:37:59 +0800

    update

--- run/train_path.sh
+++ run/train_path.sh
@@ -1,7 +1,6 @@
-python3 -u ./src/train_dg3.py \
+python3 ./src/train_dg3.py \
  --data_dir ./data/dg3_all \
  --circuit_path ./data/dg3_all/graphs.npz \
  --pretrained_model_path ./trained/model_last.pth \
  --tf_arch path \
  --sample_path_data --enable_large_circuit \
- >> ./exp/data.log 
