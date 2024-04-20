6dfaeac1e7bbe31d30427b5385951e093cd79253
zshi0616 <zshi0616@link.cuhk.edu.hk>
2024-04-20 21:34:01 +0800

    update

--- run/train.sh
+++ run/train.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-NUM_PROC=8
-GPUS=0,1,2,3,4,5,6,7
+NUM_PROC=4
+GPUS=0,1,2,3
 DATA_RATIO=100
 
-python3 -m torch.distributed.launch --nproc_per_node=$NUM_PROC --master_port=29500 ./src/train_dg3.py \
+python3 -m torch.distributed.launch --nproc_per_node=$NUM_PROC --master_port=29502 ./src/train_dg3.py \
  --exp_id train_${DATA_RATIO}p \
  --data_dir /home/zyshi21/data/inmemory/dg3_train_${DATA_RATIO}p \
  --npz_dir /home/zyshi21/data/share/dg3_dataset/${DATA_RATIO}p \
- --pretrained_model_path ./trained/model_last.pth \
+ --pretrained_model_path ./trained/model_last_workload.pth \
  --workload --gpus ${GPUS} --batch_size 128
