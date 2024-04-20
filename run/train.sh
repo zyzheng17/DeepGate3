3735f63ac920de9ba75fad0af24d7eb97a346da0
zshi0616 <zshi0616@link.cuhk.edu.hk>
2024-04-20 21:46:36 +0800

    update

--- run/train.sh
+++ run/train.sh
@@ -3,20 +3,9 @@ NUM_PROC=4
 GPUS=0,1,2,3
 DATA_RATIO=100
 
-<<<<<<< HEAD
 python3 -m torch.distributed.launch --nproc_per_node=$NUM_PROC --master_port=29502 ./src/train_dg3.py \
  --exp_id train_${DATA_RATIO}p \
  --data_dir /home/zyshi21/data/inmemory/dg3_train_${DATA_RATIO}p \
  --npz_dir /home/zyshi21/data/share/dg3_dataset/${DATA_RATIO}p \
  --pretrained_model_path ./trained/model_last_workload.pth \
  --workload --gpus ${GPUS} --batch_size 128
-=======
-nohup python3 -m torch.distributed.launch --nproc_per_node=$NUM_PROC --master_port=29557 ./DeepGate3-Transformer/src/train_dg3.py \
- --exp_id train_${DATA_RATIO}p \
- --data_dir /home/zyshi21/data/inmemory/dg3_train_${DATA_RATIO}p \
- --npz_dir /home/zyshi21/data/share/dg3_dataset/${DATA_RATIO}p \
- --pretrained_model_path ./DeepGate3-Transformer/trained/model_last_workload.pth \
- --tf_arch plain --lr 1e-4 \
- --workload --gpus ${GPUS} --batch_size 128 \
- >> /home/zyzheng23/project/DeepGate3-Transformer/exp/0418_plain_workload_80k_balanced.log 2>&1 &
->>>>>>> abf04d08c334ccf39e744812cf9fe1117cf6dc9e
